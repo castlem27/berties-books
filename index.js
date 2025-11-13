635be76f288c9f16224862b3baf76568076a61f4
castlem27 <marlon.castle087@gmail.com>
2025-11-13 16:51:41 +0000

    Added to database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Import express and ejs
 var express = require ('express')
 var ejs = require('ejs')
+var mysql = require('mysql2');
 const path = require('path')
 
 // Create the express application object
@@ -18,6 +19,18 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 // Define our application-specific data
 app.locals.shopData = {shopName: "Bertie's Books"}
+
+// Define the database connection pool
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'berties_books_app',
+    password: 'qwertyuiop',
+    database: 'berties_books',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+});
+global.db = db;
 
 // Load the route handlers
 const mainRoutes = require("./routes/main")
